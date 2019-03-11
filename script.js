ff99d4eea2412ce55c1f47d2ad963c2d31a72476
Daniela Ortenzi <daniela.ortenzi92@gmail.com>
2019-03-11 14:24:00 +0100

    finito

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function testAddMessage(){
 
-  var wrapper = $('.wrapper');
-  var input = $('#usr-input')
+  var chat = $('.selected-chat');
+  var input = $('#text-message');
   var mex = input.val();
   var message = document.createElement('div');
   var messageContent = document.createElement('p');
@@ -10,18 +10,50 @@ function testAddMessage(){
 
   $(message).addClass('message sent');
   $(messageContent).text(mex);
-  $(messageDetail).text('dettaglio messaggio dinamico');
+  $(messageDetail).text('17:10');
 
   message.append(messageContent);
   message.append(messageDetail);
 
-  wrapper.append(message);
+  chat.append(message);
+}
+
+function clearInput(){
+  var input = $('#text-message');
+  input.val("");
+}
+
+
+function textEnterEvent(e){
+  if (e.which == 13) {
+    testAddMessage();
+    clearInput();
+
+    setTimeout(function() {
+      var chat = $('.selected-chat');
+      var message = document.createElement('div');
+      var messageContent = document.createElement('p');
+      var messageDetail = document.createElement('small');
+
+
+      $(message).addClass('message received');
+      $(messageContent).text("ok, va bene!");
+      $(messageDetail).text('17:10');
+
+      message.append(messageContent);
+      message.append(messageDetail);
+
+      chat.append(message);
+    },3000 );
+  }
 }
 
 
 function init(){
-  var btn = $('#button');
-  btn.click(testAddMessage);
+
+  var text = $('#text-message');
+  text.keyup(textEnterEvent)
+
 }
 
 $(document).ready(init);
